07f3e9f8ac8049dc38224af7cc7295efb98cfd2f
Juan Velasquez <juodave@gmail.com>
2015-04-16 18:21:09 -0700

    Add solarized theme

--- bootstrap.sh
+++ bootstrap.sh
@@ -47,7 +47,7 @@ setup_gitconfig () {
     read -e git_authorname
     user ' - What is your github author email?'
     read -e git_authoremail
-    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" symlinks/gitconfig-private.example > ~/.gitconfig-private
+    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" $DOTFILES_ROOT/gitconfig-private.example > ~/.gitconfig-private
     success 'gitconfig'
   fi
 }
