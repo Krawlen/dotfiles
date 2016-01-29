35be9ef71cb55221e6d289889d54948e0d8bec14
Juan Velasquez <jvelasquez@parallel6.com>
2016-01-28 18:30:08 -0800

    Document new tools/libs

---
+++ libs/extras.sh
@@ -0,0 +1,23 @@
+##------------------Noti----------------------------
+# https://github.com/variadico/noti
+# Display a notification after a terminal process finishes.
+# Just put noti at the beginning or end of your regular commands.
+# noti [options] [utility [args...]]
+# OS X
+#curl -LOk https://github.com/variadico/noti/releases/download/v1.3.0/noti1.3.darwin-amd64.tar.gz
+
+# Linux
+#curl -LOk https://github.com/variadico/noti/releases/download/v1.3.0/noti1.3.linux-amd64.tar.gz
+
+
+##------------------Nativefier----------------------------
+
+
+#Nativefier is a command line tool that allows you to easily create a desktop application for any web site with succinct and
+#minimal configuration. Apps are wrapped by Electron in an OS executable (.app, .exe, etc.) for use on Windows, OSX and Linux.
+#https://www.npmjs.com/package/nativefier
+
+# nativefier --app-name "WhatsApp" "https://web.whatsapp.com/" --overwrite --icon <icon-path> --badge --counter
+# nativefier --app-name "Facebook Messenger" "https://www.messenger.com/" --overwrite --icon <icon-path> --badge --counter
+# nativefier --app-name "Hangouts" "https://hangouts.google.com/" --overwrite --icon <icon-path> --badge --counter
+# nativefier --app-name "Jira" "https://hangouts.google.com/" --overwrite --icon <icon-path> --badge --counter
