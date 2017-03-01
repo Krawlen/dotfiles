87f1158e2c2ee8409137632ad961e243be57dc43
Juan Velasquez <juodave@gmail.com>
2017-02-28 22:51:20 -0800

    Add Brew Casks

--- os/osx/brew_libs.sh
+++ os/osx/brew_libs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Ask for the administrator password upfront.
+# Make sure XCode is up to date!!!
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
@@ -21,10 +22,12 @@ fi
 brew upgrade
 
 # MYSQL
-# brew install mysql # Cant use mysql 5.7
-brew install mysql55
+# brew install mysql
+brew install mysql
 # Python
 brew install python
+# Python3
+brew install python3
 # ImageMagick
 brew install imagemagick
 # Macvim
@@ -49,6 +52,11 @@ brew install the_silver_searcher
 brew install noti
 # Install Node
 brew install npm
+# Install JS Plugins!
+npm install -g tern
+npm install -g jslint
+npm install -g jshint
+
 #Install qt
 brew install qt
 # Install Command-line Fuzzy Finder
@@ -57,17 +65,39 @@ brew install fzf
 /usr/local/opt/fzf/install
 # htop - Monitoring tool
 brew install htop
+# Vim replacement
+brew install neovim/neovim/neovim
+# Install neovim python 3 plugin
+pip3 install neovim
 #web proxy -----****
 #brew install mitmproxy
 
 
+### INSTALL CASKS
+brew cask install google-chrome
+brew cask install atom
+brew cask install contexts
+brew cask install iterm2
+brew cask install sourcetree
+brew cask install spotify
+brew cask install vlc
+brew cask install libreoffice
+brew cask install wmail
+brew cask install bettertouchtool
+brew cask install sequel-pro
+brew cask install whatsapp
+brew cask install messenger
+brew cask install google-hangouts
+brew cask install slack
+brew cask install android-studio
+brew cask install charles
+brew cask install postman
+
+
 # Set zsh as the default interpreter
 if [ -z "$(grep '/usr/local/bin/zsh' /etc/shells)" ];then
-  echo "/usr/local/bin/zsh" >> "/etc/shells"
+ echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
 fi
 chsh -s /usr/local/bin/zsh
 
 # Remove outdated versions from the cellar.
-brew cleanup
-
-
