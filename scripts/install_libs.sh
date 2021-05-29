ad015478d82384ae5e590c11ea006d9037045003
Juan Velasquez <jvelasquez@parallel6.com>
2021-05-28 17:51:42 -0700

    Update to include kubernetes tools

--- scripts/install_libs.sh
+++ scripts/install_libs.sh
@@ -10,6 +10,28 @@ install_dependencies(){
   mkdir -p "$HOME/.vim/bundle/"
   link_file "$(pwd)/libs/Vundle.vim" "$HOME/.vim/bundle/Vundle.vim"
 
+
+  if [ ! -f /usr/local/bin/kubectx ]; then
+    sudo ln -s "$(pwd)/libs/kubectx/kubectx" "/usr/local/bin/kubectx"
+  fi
+
+  if [ ! -f /usr/local/bin/kubens ]; then
+    sudo ln -s "$(pwd)/libs/kubectx/kubens" "/usr/local/bin/kubens"
+  fi
+
+  # mkdir -p ~/.oh-my-zsh/completions
+  # chmod -R 755 ~/.oh-my-zsh/completions
+  #link_file "$(pwd)/libs/kubectx/completion/kubectx.zsh ~/.oh-my-zsh/completions/_kubectx.zsh
+  #link_file "$(pwd)/libs/kubectx/completion/kubens.zsh ~/.oh-my-zsh/completions/_kubens.zsh
+  if [ ! -f /usr/share/zsh/functions/Completion/kubectx.zsh ]; then
+    sudo ln -s "$(pwd)/libs/kubectx/completion/kubectx.zsh" /usr/share/zsh/functions/Completion/_kubectx.zsh
+  fi
+
+  if [ ! -f /usr/share/zsh/functions/Completion/kubens.zsh ]; then
+    sudo ln -s "$(pwd)/libs/kubectx/completion/kubens.zsh" /usr/share/zsh/functions/Completion/_kubens.zsh
+  fi
+
+
   # If we're on a Mac, let's install and setup homebrew.
   #if [ "$(uname -s)" == "Darwin" ]
   #  then
