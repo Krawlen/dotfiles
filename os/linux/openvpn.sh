4358a13356ad7294476d879f52d7ec1633b8a327
Juan Velasquez <velasquezjuan@prahs.com>
2021-12-20 23:14:13 -0800

    Add open vpn install script
    This script will install openvpn3

---
+++ os/linux/openvpn.sh
@@ -0,0 +1,7 @@
+sudo apt install -y apt-transport-https
+sudo wget https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub
+sudo apt-key add openvpn-repo-pkg-key.pub
+DISTRO=$(lsb_release -c -s)
+sudo wget -O /etc/apt/sources.list.d/openvpn3.list https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-$DISTRO.list
+sudo apt update
+sudo apt install openvpn3
