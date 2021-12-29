ef04494c94ea7daaca757b6de0bec9860a836cf4
Juan <velasquezjuan@prahs.com>
2021-12-29 14:15:34 -0800

    Update linux apps

--- os/linux/install_snaps.sh
+++ os/linux/install_snaps.sh
@@ -1,16 +1,19 @@
 # Ask for the administrator password upfront.
-# Make sure XCode is up to date!!!
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
-snap install 1password
+add-apt-repository -y ppa:appimagelauncher-team/stable
+apt-get update
+apt install -y appimagelauncher
+
+# snap install 1password Install manually because when it is sandboxed it cannot communicate with browser
 snap install spotify
 snap install slack --classic
 snap install vlc
-snap install beekeeper-studio
+#snap install beekeeper-studio --classic # Install AppImage instead as the snap is not allowed to connect using SSH ( security)
 snap install code --classic
 snap install gitkraken --classic
 snap install postman
