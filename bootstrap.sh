1845f63034ecac2e973148c2c5ac4c6c216734d1
Juan <juan.velasquez@firstup.io>
2022-10-27 17:32:14 +0000

    Test for using bootstrap in dockerfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,15 @@
 #
 # bootstrap installs things.
 # DO NOT source bootstrap.sh, it is only necessary to call ./bootstrap.sh
-# Make sure to run in the same folder as the bootstrap.sh
+#
 
-# Usage: ./bootstrap.sh 
-# Auto overwrite files: ./bootstrap.sh true
-
-DOTFILES_ROOT=$( cd "$( dirname "$0" )" && pwd )/symlinks
+if [ -z "$1" ]; then
+  DOTFILES_ROOT=$( cd "$( dirname "$0" )" && pwd )/symlinks
+elif [ -d "$1" ]; then
+  DOTFILES_ROOT="$1"
+else
+  echo "Not a valid directory"
+fi
 
 source ./scripts/link_files.sh
 source ./scripts/utils.sh
@@ -20,7 +23,7 @@ echo ''
 
 #setup_gitconfig
 install_dependencies
-ruby ./scripts/install_dotfiles.rb $DOTFILES_ROOT $1
+ruby ./scripts/install_dotfiles.rb $DOTFILES_ROOT
 
 
 
