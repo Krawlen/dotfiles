b96b40a7e0933669df142b0e77f2163ed86ecaf4
Juan <juan.velasquez@firstup.io>
2023-09-21 17:33:41 -0700

    Update for compatibility

--- libs/install_neovim.sh
+++ libs/install_neovim.sh
@@ -1,9 +1,10 @@
 # Install Ruby
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-rvm install 3.0
-rvm use default 3.0
-gem install neovim
+#gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+#\curl -sSL https://get.rvm.io | bash -s stable --ruby
+#rvm install 3.0
+#rvm use default 3.0
+#gem install neovim
+brew install rbenv
 
 
 # NODE -----------------
